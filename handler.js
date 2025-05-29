c7e5688ab88fc827d37321fada74a8a156cd060f
vikasM30 <mishraviki.mishra@gmail.com>
2025-05-29 20:48:59 +0530

    A lambda function for getting you nationality and ip details

---
+++ handler.js
@@ -0,0 +1,48 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello Welcome to Lambda Serverless Functions. This function has two endpoints named: /getIPDetails?ip=<IP_ADDRESS> and /getNationality?name=<John>",
+  });
+});
+
+app.get('/getIPDetails', async (req, res) => {
+  try {
+    const {ip} = req.query;
+    const response = await fetch(`https://ipinfo.io/${ip}/geo`);
+    const val = await response.json();
+    res.status(200).json({IP_Data: val});
+  } catch (err) {
+    console.error('Route error:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/getNationality', async (req, res) => {
+  try {
+    const {name} = req.query;
+    const response = await fetch(`https://api.nationalize.io/?name=${name}`);
+    const val = await response.json();
+    res.status(200).json({nationality_data: val});
+  } catch (err) {
+    console.error('Route error:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Endpoint Not Found",
+  });
+});
+
+app.use((err, req, res, next) => {
+  console.error("Error:", err); // This will show up in CloudWatch
+  res.status(500).json({ error: "Internal Server Error" });
+});
+
+module.exports.handler = serverless(app);
